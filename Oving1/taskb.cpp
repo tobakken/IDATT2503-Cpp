b58ce2460d9c49429c937c85e3a2246a7b7bba0e
Tor Oyvind Bakken <tobakken@pop-os.localdomain>
2022-08-28 02:35:40 +0200

    Task b complete (scuffed)

--- Oving1/taskb.cpp
+++ Oving1/taskb.cpp
@@ -1,12 +1,47 @@
+#include <fstream>
 #include <iostream>
 
 using namespace std;
-void read_temperatures();
+void read_temperatures(double temperatures[], int length);
 
 int main() {
-  read_temperatures();
+  double temperatures[5];
+  int length = 5;
+  read_temperatures(temperatures, length);
+  int under = 0;
+  int between = 0;
+  int over = 0;
+
+  for (int i = 0; i < length; i++) {
+    int temp = temperatures[i];
+    if (temp > 20) {
+      over++;
+    } else if (temp > 10) {
+      between++;
+    } else {
+      under++;
+    }
+  }
+  cout << "Antall under 10 er " << under << "\n";
+  cout << "Antall mellom 10 og 20 er " << between << "\n";
+  cout << "Antall over 20 er " << over << "\n";
 }
 
-void read_temperatures() {
-  cout << "Hello world \n";
+void read_temperatures(double temperatures[], int length) {
+  const char filename[] = "numbers.dat";
+  ifstream file;       // definerer filvariabel
+  file.open(filename); // åpner filen
+  if (!file) {         // innfil kan brukes som et logisk uttrykk
+    cout << "Feil ved åpning av innfil." << endl;
+    exit(EXIT_FAILURE); // uthopp fra programmet
+  }
+
+  int number;
+  for (int i = 0; i < length; i++) {
+    file >> number;
+    temperatures[i] = number;
+    cout << temperatures[i] << endl;
+  }
+
+  file.close();
 }
